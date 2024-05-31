39f38ddc1515786cfd3383004104c84e2fcfc290
felipe-coletti <felipecoletti2@gmail.com>
2024-05-31 14:25:55 -0300

    Add a title to the component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
     return(
         <div className='container'>
+            <h1 className='title'>Classificação</h1>
             <div className='content-area'>
                 <p className='paragraph'>{rating}</p>
                 <Rating value={rating} onChange={setRating}/>
